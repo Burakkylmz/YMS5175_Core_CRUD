32676231c0aa867d1e736224569929bbb4f02d22
Burak Yılmaz <burakk.ylmaz@outlook.com>
2020-09-17 13:32:05 +0300

    Create a category entity and open up category controller

---
+++ YMS5175_Core_CRUD/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using YMS5175_Core_CRUD.DataAccess.Context;
+
+namespace YMS5175_Core_CRUD.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public ProjectContext _projectContext;
+
+        public CategoryController(ProjectContext projectContext)
+        {
+            _projectContext = projectContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ YMS5175_Core_CRUD/DataAccess/ORM/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YMS5175_Core_CRUD.DataAccess.ORM.Entities
+{
+    public class Category:BaseEntity
+    {
+    }
+}
